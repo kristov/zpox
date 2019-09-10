fe32a2b4f71c91c5db4c9d5bc26b52d124999ef7
ceade <chris.eade@gmail.com>
2019-09-10 09:21:19 +0200

    Some abstraction over finding threads in statuss

--- asm/kernel/04-tidtable.asm
+++ asm/kernel/04-tidtable.asm
@@ -5,9 +5,8 @@
 ;   function is to take a tid and turn it into an address.
 ;
 ; Usage:
-;   1) put the tid into b
-;   2) call "k_tid_addr_of"
-;   3) the address is in ix
+;   1) call "k_tid_addr_of"
+;   2) the address is in ix
 ;
 ; Explanation:
 ;   Only bc and de can be added to ix. However neither of them can be added to
@@ -18,7 +17,7 @@
 ;
 ; Registers used:
 ;
-;   b:  the current tid
+;   a:  the current tid
 ;   hl: multiplication of tid
 ;   de: transfer hl to ix
 ;   ix: address of tid entry in memory
@@ -26,30 +25,32 @@
 k_tid_addr_of:
     ld ix, k_tid_tab_base   ; put base address in ix
     ld h, 0x00              ; zero h ("xor h" didnt seem to work)
-    ld l, b                 ; put tid into lower byte
+    ld a, (k_tid_curr)      ; load current tid into a
+    ld l, a                 ; move it to l
     add hl, hl              ; x2
     add hl, hl              ; x4
     add hl, hl              ; x8
     add hl, hl              ; x16
     ex de, hl               ; de does not have "add de, de"
     add ix, de              ; add de to ix
+    ex de, hl               ; swap back as de might have useful things
     ret
 
-; k_tid_next_free(): Find the next free tid entry after tid
+; k_tid_find_status(): Find the next tid entry after tid with a given status
 ;
 ; Purpose:
 ;   The thread table can be fragmented, with empty spaces between valid threads
 ;   (paused or running). This function is to find the next entry after the
-;   current tid that is free. If the current tid is greater than zero and no
-;   free threads are found after that tid, the code wraps back around and
-;   searches from the start of the thread table back up to tid. If we always
-;   search from the beginning early threads would hog the CPU.
+;   current tid that has a certain status. If the current tid is greater than
+;   zero and no free threads are found after that tid, the code wraps back
+;   around and searches from the start of the thread table back up to tid. If
+;   we always search from the beginning early threads would hog the CPU.
 ;
 ; Usage:
-;   1) put the tid into b
-;   2) call "k_tid_next_free"
-;   3) if c != 0 then ix == address of tid and c == next available tid
-;   4) if c == 0 then no free space was found (tid 0 is reserved)
+;   1) put the desired status into c
+;   2) call "k_tid_find_status"
+;   3) if l != 0 then ix == address of tid and l == the new tid
+;   4) if l == 0 then no thread of that status was found (tid 0 is reserved)
 ;
 ; Explanation:
 ;   First k_tid_addr_of() is called to set ix to the address of the thread table
@@ -57,7 +58,7 @@ k_tid_addr_of:
 ;   started to scan all possible table entries. Each loop adds de to ix to move
 ;   to the next entry. Unless tid is zero this loop would extend beyond the
 ;   table. So the tid is subtracted from k_tid_max to give the point where
-;   we reset ix to the beginning of the table and reset c to 0. This means we
+;   we reset ix to the beginning of the table and reset l to 0. This means we
 ;   will scan up from the current tid looking for the next free entry, and when
 ;   we get to the top it will cycle back to the beginning of the table. Because
 ;   b is controlling the loop we will stop before getting back to the current
@@ -67,33 +68,49 @@ k_tid_addr_of:
 ;
 ;   a:  misc
 ;   b:  loop variable for the thread table
-;   c:  The return tid
+;   l:  The return tid
 ;   h:  number of entries from current tid until end of table
 ;   de: size of a block
 ;   ix: address of tid entry in memory
 ;
-k_tid_next_free:
-    ld c, b                 ; save the current tid into c
+k_tid_find_status:
     call k_tid_addr_of      ; calculate the address of the current tid in the table
-    ld de, k_tid_tab_len    ; set de to size of block
-    ld b, k_tid_max         ; prepare to loop around k_tid_max times
+    ld a, (k_tid_curr)      ; load current tid into a
+    ld l, a                 ; move it to l
     ld a, k_tid_max         ; calculate the diff betwen tid and the max table entry
-    sub c                   ; a now contains k_tid_max - tid (nr of loops before resetting k_tid_tab_base)
+    sub l                   ; a now contains k_tid_max - tid (nr of loops before resetting k_tid_tab_base)
     ld h, a                 ; free up a, h becomes loops until a reset of ix needed
+    ld b, k_tid_max         ; prepare to loop around k_tid_max times
+    ld de, k_tid_tab_len    ; set de to size of block
 k_tid_next_block:
     add ix, de              ; shift ix to next block (de is k_tid_tab_len)
-    inc c                   ; increment the new tid stored in c
+    inc l                   ; increment the new tid stored in l
     dec h                   ; decrement h
     jp nz, k_tid_no_reset   ; if h is not zero skip resetting ix
     ld ix, k_tid_tab_base   ; cycle ix back to beginning of table
-    ld c, 0x00              ; cycle new tid back around
+    ld l, 0x00              ; cycle new tid back around
 k_tid_no_reset:
     ld a, (ix+0)            ; load status byte into a
-    and a                   ; trigger zero flag if zero
-    jp z, k_tid_found       ; found free tid
+    sub c                   ; subtract the wanted status
+    jp z, k_tid_found       ; if zero we found an entry
     djnz k_tid_next_block
-    ld c, 0x00              ; not found
+    ld l, 0x00              ; not found
 k_tid_found:
+    ret
+
+; k_tid_next_free(): Find the next empty thread table entry
+;
+; Purpose:
+;   Finds the tid and address of the next empty thread.
+;
+; Usage:
+;   1) call "k_tid_next_free"
+;   2) if c != 0 then ix == address of tid and c == next runnable tid
+;   3) if c == 0 then no runnable threads were found (tid 0 is reserved)
+;
+k_tid_next_free:
+    ld c, 0x00              ; looking for status zero meaning free
+    call k_tid_find_status  ; use status search routine
     ret
 
 ; k_tid_next_run(): Find the next runnable thread
@@ -102,14 +119,12 @@ k_tid_found:
 ;   Finds the tid and address of the next runnable thread.
 ;
 ; Usage:
-;   1) put the tid into b
-;   2) call "k_tid_next_run"
-;   3) if c != 0 then ix == address of tid and c == next runnable tid
-;   4) if c == 0 then no runnable threads were found (tid 0 is reserved)
-;
-; Explanation:
+;   1) call "k_tid_next_run"
+;   2) if c != 0 then ix == address of tid and c == next runnable tid
+;   3) if c == 0 then no runnable threads were found (tid 0 is reserved)
 ;
 k_tid_next_run:
-    ld c, b
+    ld c, 0x01              ; look for running status
+    call k_tid_find_status  ; use status search routine
     ret
 
