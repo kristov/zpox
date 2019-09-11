1017af060ef01f4e8f8b3e71fdc3837a3a7c7cab
ceade <chris.eade@gmail.com>
2019-09-11 21:12:24 +0200

    Process state variables

--- asm/kernel/00-variables.asm
+++ asm/kernel/00-variables.asm
@@ -29,3 +29,25 @@ k_proc4_sp: equ 0x5000      ; stack pointer for proc 4
 k_proc5_sp: equ 0x6000      ; stack pointer for proc 5
 k_proc6_sp: equ 0x7000      ; stack pointer for proc 6
 k_proc7_sp: equ 0x8000      ; stack pointer for proc 7
+
+; Kernel constants
+;
+; Thread table states (lower two bits of first byte in thread table)
+;
+k_t_unused: equ 0x00        ; this thread table entry is free
+k_t_running: equ 0x01       ; the thread is running
+k_t_blocked: equ 0x02       ; the thread is blocked
+k_t_ready: equ 0x03         ; the thread can be run next
+
+; Signals
+;
+k_sighup: equ 0x01          ; terminate
+k_sigint: equ 0x02          ; terminate
+k_sigpipe: equ 0x03         ; terminate
+k_sigchld: equ 0x04         ; ignore
+k_sigstop: equ 0x05         ; block
+k_sigcont: equ 0x06         ; run
+k_sigtstp: equ 0x07         ; block
+k_sigttin: equ 0x08         ; block
+k_sigttou: equ 0x09         ; block
+
