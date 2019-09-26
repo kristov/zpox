c58545554fde28c4a18b8b7a66517c7193d96a2e
ceade <chris.eade@gmail.com>
2019-09-26 08:24:43 +0200

    Fixed test for wrap call

--- asm/kernel/t/04-tidtable/01-wrap.asm
+++ asm/kernel/t/04-tidtable/01-wrap.asm
@@ -4,27 +4,18 @@
 include '../../00-variables.asm'
 
 main:
-    ld hl, 0xff             ; set entry 0 to in-use
-    ld (k_tid_tab_base), hl ; mark zeroth in use
-    ld a, 0x02              ; copy 2 blocks, leave 106 free
-    ld hl, 0x0125           ; load hl
-copy:
-    ld bc, k_tid_tab_len    ; amount of data
-    ex de, hl               ; copy hl to de
-    ld hl, data             ; src test data
-    ldir
-    ex de, hl               ; copy hl to de
-    add hl, bc              ; increment hl by bc
-    dec a                   ; count down
-    jr z, test              ; begin test once X blocks are loaded
-    jr copy                 ; goto copy
+    ld a, k_t_running       ; set a to running state value
+    ld de, k_tid_tab_base   ; set de to location of table
+    ld (de), a              ; set status byte to in-use
+    inc de                  ; go to next entry
+    inc de                  ; skip over entry 1 (leave it zero)
+    ld (de), a              ; set status byte to in-use
+    inc de                  ; go to next entry
+    ld (de), a              ; set status byte to in-use
 test:
-    ld a, 0x02              ; prepare to load 0 at location
-    ld (k_tid_curr), a      ; current tid == 0
-    call k_tid_next_free    ; should get 1 loaded in l
+    ld a, 0x02              ; prepare to load 2 at location
+    ld (k_tid_curr), a      ; current tid == 2
+    call k_tid_next_free    ; should skip over tid 3 and 0 and return 1
     halt
 
 include '../../04-tidtable.asm'
-
-data:
-    defb 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0x02
