d67500bf4a84a03b663a5e7bcc637144eac32e6f
ceade <chris.eade@gmail.com>
2019-09-29 08:24:23 +0200

    Prepare to move some things around

--- asm/kernel/kernel.asm
+++ asm/kernel/kernel.asm
@@ -1,6 +1,6 @@
 include '00-variables.asm'
-include '01-rst00.asm'
-include '02-rst38.asm'
+include '01-interrupts.asm'
+include '02-threadman.asm'
 include '03-bootstrap.asm'
 include '04-tidtable.asm'
 include '05-syscalls.asm'
